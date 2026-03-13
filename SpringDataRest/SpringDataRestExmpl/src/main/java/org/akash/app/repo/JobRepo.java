4760cca257f5f9496f30d195dba844b749afe9b0
Akash Samanta <137557300+PCodAlex@users.noreply.github.com>
2026-03-13 16:48:50 +0530

    spring data rest project added

---
+++ SpringDataRest/SpringDataRestExmpl/src/main/java/org/akash/app/repo/JobRepo.java
@@ -0,0 +1,13 @@
+package org.akash.app.repo;
+
+//import java.util.List;
+
+import org.akash.app.model.JobPost;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface JobRepo extends JpaRepository<JobPost, Integer>{
+	//List<JobPost> findByPostProfileContainingOrPostDescContaining(String postProfile,String postDesc);
+}
+	
